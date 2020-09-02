1be91912247bd2aea24f9dae3331b228aef1bd94
Dhruvil <modidhruvil42@gmail.com>
2020-09-02 11:40:06 +0530

    html and views added with hyperlinks

--- employee/employee_app/urls.py
+++ employee/employee_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    #path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path("departments/<int:branch_id>",views.depart_info,name="departments"),
+    path("employees/<int:department_id>",views.employee_info,name="employees")
 ]
--- employee/employee_app/views.py
+++ employee/employee_app/views.py
@@ -5,5 +5,21 @@ from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
-    return render()
+    branches=branch.objects.all()
+    return render(request,'employee_app/index.html',{
+        "brn":branches
+    })
 
+def depart_info(request,branch_id):
+    branchdetail=branch.objects.get(pk=branch_id);
+    departs=branchdetail.departs.all()
+    return render(request,'employee_app/departs.html',{
+        "depts":departs
+    })
+
+def employee_info(request,department_id):
+    department_details=departments.objects.get(pk=department_id)
+    employeee=department_details.workers.all()
+    return render(request,'employee_app/emp.html',{
+        "emps":employeee
+    })
